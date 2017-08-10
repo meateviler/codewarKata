bed92a5db3e61ca2be9faf6c876e8f0a9b40b8e5
meateviler <meateviler@msn.com>
2017-08-10 18:31:27 +0800

    add PalindromeChainLength

---
+++ PalindromeChainLength/PalindromeChainLength/kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PalindromeChainLength
+{
+	public static class Kata
+	{
+		public static int palindromeChainLength(int n)
+		{
+			var step = 0;
+			long tmp = n;
+			while (!IsPalindrome(tmp))
+			{
+				tmp = tmp + ReverseNumber(tmp);
+				step++;
+			}
+
+			return step;
+		}
+
+		private static bool IsPalindrome(long n)
+		{
+			return n == ReverseNumber(n);
+		}
+
+		private static long ReverseNumber(long n)
+		{
+			var arrayN = n.ToString().ToCharArray();
+			Array.Reverse(arrayN);
+			return Convert.ToInt64(new string(arrayN));
+		}
+	}
+}
---
+++ PalindromeChainLength/PalindromeChainLengthTests/KataTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace PalindromeChainLength.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase(5, ExpectedResult = 0, TestName = "5 should be 0 steps")]
+		[TestCase(44, ExpectedResult = 0, TestName = "44 should be 0 steps")]
+		[TestCase(171, ExpectedResult = 0, TestName = "171 should be 0 steps")]
+		[TestCase(87, ExpectedResult = 4, TestName = "87 should be 4 steps")]
+		[TestCase(89, ExpectedResult = 24, TestName = "89 should be 24 steps")]
+		public int palindromeChainLengthTest(int num)
+		{
+			return Kata.palindromeChainLength(num);
+		}
+	}
+}
