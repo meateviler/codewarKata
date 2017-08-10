ad362745d8be4b623475468dd8a475195187f2a6
meateviler <meateviler@msn.com>
2017-08-10 18:43:15 +0800

    add DuplicateCount

---
+++ DuplicateCount/DuplicateCount/Kata.cs
@@ -0,0 +1,26 @@
+﻿namespace DuplicateCount
+{
+	public class Kata
+	{
+		public static int DuplicateCount(string str)
+		{
+			if (string.IsNullOrEmpty(str))
+				return 0;
+
+			var tmp = str.ToLower();
+			var currentLen = tmp.Length;
+			var duplicateCount = 0;
+			while (tmp.Length > 0)
+			{
+				tmp = tmp.Replace(tmp[0].ToString(), string.Empty);
+				if (tmp.Length + 1 < currentLen)
+				{
+					duplicateCount++;
+				}
+				currentLen = tmp.Length;
+			}
+
+			return duplicateCount;
+		}
+	}
+}
---
+++ DuplicateCount/DuplicateCountTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace DuplicateCount.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase("", ExpectedResult = 0, TestName = "Empty should be 0")]
+		[TestCase("abcde", ExpectedResult = 0, TestName = "abcde should be 0")]
+		[TestCase("aabbcde", ExpectedResult = 2, TestName = "aabbcde should be 2")]
+		[TestCase("aabBcde", ExpectedResult = 2, TestName = "aabBcde should be 2 ignore case")]
+		[TestCase("Indivisibility", ExpectedResult = 1, TestName = "Indivisibility should be 2")]
+		[TestCase("Indivisibilities", ExpectedResult = 2, TestName = "Indivisibilities should be 2, characters may not be adjacent")]
+		public int DuplicateCountTest(string input)
+		{
+			return Kata.DuplicateCount(input);
+		}
+	}
+}
