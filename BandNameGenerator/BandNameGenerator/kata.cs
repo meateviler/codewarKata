a34cd28ca048a2b5784cb7f8e57b376704a3bc86
meateviler <meateviler@msn.com>
2017-08-10 18:39:48 +0800

    add BandNameGenerator

---
+++ BandNameGenerator/BandNameGenerator/kata.cs
@@ -0,0 +1,43 @@
+﻿namespace BandNameGenerator
+{
+	public class Kata
+	{
+		public static string BandNameGenerator(string str)
+		{
+			var tmp = str.ToLower();
+			if (tmp.Length == 1)
+				return ConvertFirstCharToUpperAndAddThe(tmp);
+			if (IsStartAndEndSameLetter(tmp))
+			{
+				return ConvertFirstCharToUpperWithWholeWordAndRepeatOnceWithoutFirstChar(tmp);
+			}
+
+			return ConvertFirstCharToUpperWithWholeWordAndAddThe(tmp);
+		}
+
+		private static string ConvertFirstCharToUpperWithWholeWordAndRepeatOnceWithoutFirstChar(string str)
+		{
+			return ConvertFirstCharToUpper(str) + str.Substring(1, str.Length-2) + str;
+		}
+
+		private static string ConvertFirstCharToUpperWithWholeWordAndAddThe(string str)
+		{
+			return ConvertFirstCharToUpperAndAddThe(str) + str.Substring(1, str.Length - 1);
+		}
+
+		private static bool IsStartAndEndSameLetter(string str)
+		{
+			return str[0] == str[str.Length - 1];
+		}
+
+		private static string ConvertFirstCharToUpper(string str)
+		{
+			return str[0].ToString().ToUpper();
+		}
+
+		private static string ConvertFirstCharToUpperAndAddThe(string str)
+		{
+			return "The " + ConvertFirstCharToUpper(str);
+		}
+	}
+}
---
+++ BandNameGenerator/BandNameGeneratorTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace BandNameGenerator.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase("k", ExpectedResult = "The K", TestName = "k_Should_be_The K")]
+		[TestCase("aa", ExpectedResult = "Aaa", TestName = "aa_Should_be_The Aaa")]
+		[TestCase("knife", ExpectedResult = "The Knife", TestName = "knife_Should_be_The Knife")]
+		[TestCase("tart", ExpectedResult = "Tartart", TestName = "tart_Should_be_Tartart")]
+		[TestCase("sandles", ExpectedResult = "Sandlesandles", TestName = "sandles_Should_be_The Sandlesandles")]
+		[TestCase("bed", ExpectedResult = "The Bed", TestName = "bed_Should_be_The Bed")]
+		public string BandNameGeneratorTest(string input)
+		{
+			return Kata.BandNameGenerator(input);
+		}
+	}
+}
