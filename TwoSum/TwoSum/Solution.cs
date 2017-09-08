2f264d4a07ad24ae29aae383d9a46b21d8008caa
meateviler <meateviler@msn.com>
2017-09-08 16:51:49 +0800

    Add Two Sum
    LeetCode

---
+++ TwoSum/TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+	public class Solution
+	{
+		public int[] TwoSum(int[] nums, int target)
+		{
+			for (int i = 0; i< nums.Length; i++)
+			{
+				for (int j = 0; j<i; j++)
+				{
+					if (nums[i] + nums[j]  == target)
+					{
+						return new int[] { i, j };
+					}
+				}
+			}
+			return new int[] { 0, 1 };
+		}
+	}
+}
---
+++ TwoSum/TwoSumTests/SolutionTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TwoSum.Tests
+{
+	[TestFixture()]
+	public class SolutionTests
+	{
+		[TestCase(new int[] { 2, 7, 11, 15 }, 0, ExpectedResult = new int[] { 0, 1 }, TestName = "Empty should be 0")]
+		public int[] TwoSumTest(int[] nums, int target)
+		{
+			Solution t = new Solution();
+			return t.TwoSum(nums, target);
+		}
+	}
+}
