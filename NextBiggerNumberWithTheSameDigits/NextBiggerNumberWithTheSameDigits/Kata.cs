84148a29f7fa80ff924594df618bbacd1bd3aa10
meateviler <meateviler@msn.com>
2017-10-07 17:08:46 +0800

    add Next bigger number with the same digits

---
+++ NextBiggerNumberWithTheSameDigits/NextBiggerNumberWithTheSameDigits/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace NextBiggerNumberWithTheSameDigits
+{
+	public class Kata
+	{
+		public static long NextBiggerNumber(long n)
+		{
+			var strN = n.ToString().ToArray();
+			var pivot = -1;
+			for (int i = strN.Length - 1; i >0; i--)
+			{
+				if (strN[i] > strN[i - 1])
+				{
+					pivot = i-1;
+					break;
+				}
+			}
+			if (pivot == -1)
+				return -1;
+			var leftHandNoChangeNumber = strN.Take(pivot);
+			var lowestNumButGreatThanPivot = strN.Skip(pivot).Where(x => x>strN[pivot]).Min();
+			var rightHandSohouldBeReOrder = strN.Skip(pivot).ToList();
+			rightHandSohouldBeReOrder.Remove(lowestNumButGreatThanPivot);
+			var afterReOrder = string.Join("", rightHandSohouldBeReOrder.OrderBy(x => x).ToArray());
+			var strValue = string.Join("", leftHandNoChangeNumber.ToArray());
+			strValue = string.Concat(string.Concat(strValue, lowestNumButGreatThanPivot), afterReOrder);
+			return long.Parse(strValue);
+		}
+	}
+}
---
+++ NextBiggerNumberWithTheSameDigits/NextBiggerNumberWithTheSameDigitsTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace NextBiggerNumberWithTheSameDigits.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase(12, ExpectedResult = 21, TestName = "12 shouldBe 21")]
+		[TestCase(513, ExpectedResult = 531, TestName = "513 shouldBe 531")]
+		[TestCase(2017, ExpectedResult = 2071, TestName = "2017 shouldBe 2071")]
+		[TestCase(414, ExpectedResult = 441, TestName = "414 shouldBe 441")]
+		[TestCase(144, ExpectedResult = 414, TestName = "144 shouldBe 414")]
+		[TestCase(131316994, ExpectedResult = 131319469, TestName = "131316994 shouldBe 131319469")]
+		public long NextBiggerNumberTest(long input)
+		{
+			return Kata.NextBiggerNumber(input);
+		}
+	}
+}
